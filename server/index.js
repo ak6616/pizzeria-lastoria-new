2435e3a3f7c9f094a946a116562750e614949ce7
ak6616 <miloszjozefczyk6@gmail.com>
2024-12-10 18:01:17 +0100

    update dla vite

--- server/index.js
+++ server/index.js
@@ -7,14 +7,16 @@ import session from 'express-session';
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors({
+const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://twoja-domena.vercel.app', 'http://localhost:5173']
+    ? ['https://pizzeria-lastoria.vercel.app/']
     : ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
-}));
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Dodaj obsługę sesji
