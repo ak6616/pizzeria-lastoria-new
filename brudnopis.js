1388f877bee8f86f01bea629b922ae4ca560de80
ak6616 <miloszjozefczyk6@gmail.com>
2025-04-23 23:47:56 +0200

    Update brudnopis.js

--- brudnopis.js
+++ brudnopis.js
@@ -5,12 +5,12 @@
  * -zmodyfikować zamówienia na godzinę (co najmniej godzinę do przodu)
  * ----------------------------------------
  * pracuję nad powiadomieniami push
- * powiadomienia powinny działać sprawnie
- * 
- * 
- * 
+ * powiadomienia w toku, stoją w miejscu
  * --------------------------------
- * jestem w trakcie dodawania możliwości wrzucania zdjęć na stronę
+ * blokada zamawiania w toku
+ * --------------------------------
+ * jestem w trakcie dodawania możliwości wrzucania zdjęcia na stronę
  * skopiowałem wszystkie zdjęcia
- * dodawanie zdjęć powinno być teraz o wiele prostsze
+ * dodawanie zdjęcia powinno być teraz o wiele prostsze
+ * dodawanie zdjęcia z upload gotowe
 */
