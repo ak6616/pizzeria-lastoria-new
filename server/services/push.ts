f790f7bd35611a59aeb7c1b715a694b541770677
ak6616 <miloszjozefczyk6@gmail.com>
2025-04-25 22:58:26 +0200

    Update push.ts

--- server/services/push.ts
+++ server/services/push.ts
@@ -18,18 +18,18 @@ export async function registerPush(publicKey: string) {
 
       if (!response.ok) {
         throw new Error('Failed to subscribe to push notifications');
-      }
+      };
 
       console.log('Użytkownik zapisany do powiadomień');
-    }
+    };
   } catch (error) {
-    console.error('Błąd podczas rejestracji powiadomień:', error);;
-  }
-}
+    console.error('Błąd podczas rejestracji powiadomień:', error);
+  };
+};
 
 function urlBase64ToUint8Array(base64String: string) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
   const rawData = window.atob(base64);
   return new Uint8Array([...rawData].map((char) => char.charCodeAt(0)));
-}
+};
